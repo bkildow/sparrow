ce8dac17c7897565b47d5716f9954358924c9090
Brian Kildow <bkildow@phase2technology.com>
2018-11-10 23:14:23 -0500

    Navbar work

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,46 +1,28 @@
-import React from 'react'
-import { Link } from 'gatsby'
-import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import React from "react";
+import { Link } from "gatsby";
+import logo from "../img/logo.svg";
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
     <div className="container">
       <div className="navbar-brand">
         <Link to="/" className="navbar-item">
-          <figure className="image">
-            <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
-          </figure>
+          <img src={logo} alt="Sparrow Coaching" style={{ width: "250px" }} />
         </Link>
       </div>
-      <div className="navbar-start">
+      <div className="navbar-end">
         <Link className="navbar-item" to="/about">
           About
         </Link>
-        <Link className="navbar-item" to="/products">
-          Products
+        <Link className="navbar-item" to="/coaching">
+          Coaching
         </Link>
         <Link className="navbar-item" to="/contact">
           Contact
         </Link>
-        <Link className="navbar-item" to="/contact/examples">
-          Form Examples
-        </Link>
-      </div>
-      <div className="navbar-end">
-        <a
-          className="navbar-item"
-          href="https://github.com/AustinGreen/gatsby-netlify-cms-boilerplate"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <span className="icon">
-            <img src={github} alt="Github" />
-          </span>
-        </a>
       </div>
     </div>
   </nav>
-)
+);
 
-export default Navbar
+export default Navbar;
